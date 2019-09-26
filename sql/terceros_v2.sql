8573461f8157dc19b5dfd64bf2718dfde7f7e2ee
AlaskaRising <nicostevrr@gmail.com>
2019-09-26 14:08:13 -0500

    :bird: Junto a @mariaalejandra9404 Se hacen cambios pertinentes para manejo de informacion basica de Terceros. Siguiendo recomendaciones de DBA's y usando la misma estrategia se crean tablas para persistencia de Eps - Arl - Caja compensacion - Pensiones entre otras para un tercero, lo siguiente sera reunion con DBA's para evaluar y analizar el nuevo modelo.  #1

--- sql/terceros_v2.sql
+++ sql/terceros_v2.sql
@@ -67,6 +67,54 @@ COMMENT ON COLUMN terceros.tipo_contribuyente.activo IS 'Valor booleano para ind
 COMMENT ON COLUMN terceros.tipo_contribuyente.fecha_creacion IS 'Fecha y hora de la creación del registro en la BD.';
 COMMENT ON COLUMN terceros.tipo_contribuyente.fecha_modificacion IS 'Fecha y hora de la ultima modificación del registro en la BD.';
 
+CREATE SEQUENCE terceros.tipo_info_basica_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+
+CREATE TABLE terceros.tipo_info_basica(
+	id integer NOT NULL DEFAULT nextval('terceros.tipo_info_basica_id_seq'::regclass),
+    nombre character varying(100) NOT NULL,
+	descripcion character varying(100),
+	codigo_abreviacion character varying(20),
+	activo boolean NOT NULL,
+	fecha_creacion TIMESTAMP NOT NULL,
+	fecha_modificacion TIMESTAMP NOT NULL,
+	CONSTRAINT pk_tipo_info_basica PRIMARY KEY (id)
+);
+
+-- Pendiente !!! !!! 	!!!
+COMMENT ON TABLE terceros.tipo_info_basica IS 'Tabla que parametriza los grupos de grupos sanguineo, grupos etnicos, discapacidades, estados civiles, generos entre otros.';
+
+
+CREATE SEQUENCE terceros.tipo_seguridad_social_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+
+CREATE TABLE terceros.tipo_seguridad_social(
+	id integer NOT NULL DEFAULT nextval('terceros.tipo_seguridad_social_id_seq'::regclass),
+    nombre character varying(100) NOT NULL,
+	descripcion character varying(100),
+	codigo_abreviacion character varying(20),
+	activo boolean NOT NULL,
+	fecha_creacion TIMESTAMP NOT NULL,
+	fecha_modificacion TIMESTAMP NOT NULL,
+	CONSTRAINT pk_tipo_seguridad_social PRIMARY KEY (id)
+);
+
+-- Pendiente !!! !!! 	!!!
+COMMENT ON TABLE terceros.tipo_seguridad_social IS 'Tabla que parametriza a que tipo de entidad pertenece el tercero sea:  Eps / Caja Compensacion / Arl / fondo de Pension entre otros.';
+
+
 
 --||||||||||||||||||||||||||||||||||||
 --Creacion tabla informacion_basica para:
@@ -93,7 +141,7 @@ CREATE TABLE terceros.info_basica(
 	tipo_info_basica_id integer NOT NULL,
 	fecha_creacion TIMESTAMP NOT NULL,
 	fecha_modificacion TIMESTAMP NOT NULL,
-	CONSTRAINT pk_info_basica PRIMARY KEY (id)
+	CONSTRAINT pk_info_basica PRIMARY KEY (id),
 	CONSTRAINT fk_tipo_info_basica_info_basica FOREIGN KEY (tipo_info_basica_id) REFERENCES terceros.tipo_info_basica(id)
 );
 
@@ -103,33 +151,9 @@ COMMENT ON COLUMN terceros.info_basica.id IS 'Identificador unico de la tabla WW
 COMMENT ON COLUMN terceros.info_basica.nombre IS 'Campo obligatorio de la tabla que indica el nombre del parámetro.';
 COMMENT ON COLUMN terceros.info_basica.codigo_abreviacion IS 'Código de abreviación, sigla, acrónimo u otra representación corta del registro si se requiere.';
 COMMENT ON COLUMN terceros.info_basica.activo IS 'Valor booleano para indicar si el registro esta activo o inactivo.';
-COMMENT ON COLUMN terceros.info_basica.tipo_informacion_basica_id IS 'Identificador unico de la tabla tipo_informacion_basica.';
+COMMENT ON COLUMN terceros.info_basica.tipo_info_basica_id IS 'Identificador unico de la tabla tipo_informacion_basica.';
 COMMENT ON COLUMN terceros.info_basica.fecha_creacion IS 'Fecha y hora de la creación del registro en la BD.';
 COMMENT ON COLUMN terceros.info_basica.fecha_modificacion IS 'Fecha y hora de la ultima modificación del registro en la BD.';
-
-
-CREATE SEQUENCE terceros.tipo_info_basica_id_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 9223372036854775807
-	START WITH 1
-	CACHE 1
-	NO CYCLE
-	OWNED BY NONE;
-
-CREATE TABLE terceros.tipo_info_basica(
-	id integer NOT NULL DEFAULT nextval('terceros.tipo_info_basica_id_seq'::regclass),
-    nombre character varying(100) NOT NULL,
-	descripcion character varying(100),
-	codigo_abreviacion character varying(20),
-	activo boolean NOT NULL,
-	fecha_creacion TIMESTAMP NOT NULL,
-	fecha_modificacion TIMESTAMP NOT NULL,
-	CONSTRAINT pk_tipo_info_basica PRIMARY KEY (id)
-);
-
--- Pendiente !!! !!! 	!!!
-COMMENT ON TABLE terceros.tipo_info_basica IS 'Tabla que parametriza los grupos de grupos sanguineo, grupos etnicos, discapacidades, estados civiles, generos entre otros.';
 
 
 --|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -207,7 +231,7 @@ COMMENT ON COLUMN terceros.datos_identificacion.fecha_modificacion IS 'Fecha y h
 
 
 --||||||||||||||||||||||||||||||||||||
---Creacion tabla                para relacionar:
+--Creacion tabla info_basica_tercero para relacionar:
 --               tipo_grupo_sanguineo
 --               tipo_grupo_etnico
 --               tipo_discapacidad
@@ -228,16 +252,79 @@ CREATE TABLE terceros.info_basica_tercero(
 	id integer NOT NULL DEFAULT nextval('terceros.info_basica_tercero_id_seq'::regclass),
 	tercero_id integer NOT NULL,
 	info_basica_id integer NOT NULL,
+	activo boolean NOT NULL,
 	fecha_creacion TIMESTAMP NOT NULL,
 	fecha_modificacion TIMESTAMP NOT NULL,
-	CONSTRAINT pk_info_basica_tercero PRIMARY KEY (id)
+	CONSTRAINT pk_info_basica_tercero PRIMARY KEY (id),
 	CONSTRAINT fk_tercero_info_basica_tercero FOREIGN KEY (tercero_id) REFERENCES terceros.tercero(id),
 	CONSTRAINT fk_info_basica_info_basica_tercero FOREIGN KEY (info_basica_id) REFERENCES terceros.info_basica(id)
 );
 COMMENT ON TABLE terceros.info_basica_tercero IS 'Tabla que relaciona a un Tercero con su informacion basica sea esta: ';
 
 
+CREATE SEQUENCE terceros.seguridad_social_tercero_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
 
+CREATE TABLE terceros.seguridad_social_tercero(
+	id integer NOT NULL DEFAULT nextval('terceros.seguridad_social_tercero_id_seq'::regclass),
+	tercero_id integer NOT NULL,
+	tercero_entidad_id integer NOT NULL,
+	tipo_seguridad_social_id integer NOT NULL,
+	activo boolean NOT NULL,
+	fecha_inicio_vinculacion TIMESTAMP NOT NULL,
+	fecha_fin_vinculacion TIMESTAMP NOT NULL,
+	fecha_creacion TIMESTAMP NOT NULL,
+	fecha_modificacion TIMESTAMP NOT NULL,
+	CONSTRAINT pk_seguridad_social_tercero PRIMARY KEY (id),
+	CONSTRAINT fk_tercero_seguridad_social_tercero FOREIGN KEY (tercero_id) REFERENCES terceros.tercero(id),
+	CONSTRAINT fk_tercero_entidad_seguridad_social_tercero FOREIGN KEY (tercero_entidad_id) REFERENCES terceros.tercero(id),
+	CONSTRAINT fk_tipo_seguridad_social_seguridad_social_tercero FOREIGN KEY (tipo_seguridad_social_id) REFERENCES terceros.tipo_seguridad_social(id)
+);
+COMMENT ON TABLE terceros.seguridad_social_tercero IS 'Tabla que relaciona a un Tercero con su:  Eps / Caja Compensacion / Arl / fondo de Pension entre otros.';
+
+
+
+--Creacion tabla informacion_socioeconomica 
+
+CREATE SEQUENCE terceros.informacion_socioeconomica_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START WITH 1
+	CACHE 1
+	NO CYCLE
+	OWNED BY NONE;
+
+CREATE TABLE terceros.informacion_socioeconomica(
+	id integer NOT NULL DEFAULT nextval('terceros.informacion_socioeconomica_id_seq'::regclass),
+    tercero_id integer NOT NULL,
+    estrato numeric (1,0),
+    puntaje_sisben numeric (3,2),
+    declarante_renta boolean, 
+    comunidad_lgbt boolean,
+    estado_civil boolean,
+    cabeza_familia boolean,
+    personas_a_cargo boolean,
+    numero_personas_a_cargo numeric(2,0),
+    hijos boolean,
+    numero_hijos numeric(2,0),
+	activo boolean NOT NULL,
+	fecha_creacion TIMESTAMP NOT NULL,
+	fecha_modificacion TIMESTAMP NOT NULL,
+	CONSTRAINT pk_informacion_socioeconomica PRIMARY KEY (id),
+    CONSTRAINT fk_tercero_informacion_socieconomica FOREIGN KEY (tercero_id) REFERENCES terceros.tercero(id)
+);
+
+
+
+
+/**
 
 --Creacion tabla informacion_socioeconomica 
 
@@ -302,5 +389,4 @@ COMMENT ON COLUMN terceros.informacion_socioeconomica.tercero_id IS 'Identificad
 COMMENT ON COLUMN terceros.informacion_socioeconomica.activo IS 'Valor booleano para indicar si el registro esta activo o inactivo.';
 COMMENT ON COLUMN terceros.informacion_socioeconomica.fecha_creacion IS 'Fecha y hora de la creación del registro en la BD.';
 COMMENT ON COLUMN terceros.informacion_socioeconomica.fecha_modificacion IS 'Fecha y hora de la ultima modificación del registro en la BD.';
-
-
+**/
