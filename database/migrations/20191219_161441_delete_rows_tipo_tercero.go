a68855c2785b3ce958d066f685e38a5db60ea11a
María Alejandra Ortiz <mariaalejandraortiz0401@gmail.com>
2019-12-19 16:26:26 -0500

    :dizzy: Con @mariaalejandra9404 Se agrega tipo_tercero para manejo de colegios cuya informacion sera proporcionada por un estudiante (para los casos de colegios que no se encuentren en el sistema), que requieren verificacion manual, como se acordo en reunion #40

--- database/migrations/20191219_161441_delete_rows_tipo_tercero.go
+++ database/migrations/20191219_161441_delete_rows_tipo_tercero.go
@@ -20,11 +20,13 @@ func init() {
 // Run the migrations
 func (m *DeleteRowsTipoTercero_20191219_161441) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-
+	m.SQL("DELETE FROM terceros.tipo_tercero WHERE id BETWEEN 13 AND 18;")
+	m.SQL("INSERT INTO terceros.tipo_tercero (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion) VALUES (13, 'DOCENTE', 'Docente independiente del tipo de vinculación.', 'DOCENTE', TRUE, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	
 }
 
 // Reverse the migrations
 func (m *DeleteRowsTipoTercero_20191219_161441) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
-
+	m.SQL("DELETE FROM terceros.tipo_tercero WHERE id = 13;")
 }
