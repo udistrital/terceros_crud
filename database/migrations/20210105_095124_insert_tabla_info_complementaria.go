c329b2654181fd0877bece2d7397d68435f7372f
criss201x <cryteck_1011@hotmail.com>
2021-01-27 15:59:17 -0500

    fix: junto con @JocoolQ se agrega registro en tabla grupo_info_complementaria correspondiente al id 18 que hace referencia a formacion academica #70

--- database/migrations/20210105_095124_insert_tabla_info_complementaria.go
+++ database/migrations/20210105_095124_insert_tabla_info_complementaria.go
@@ -20,6 +20,7 @@ func init() {
 // Run the migrations
 func (m *InsertTablaInfoComplementaria_20210105_095124) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("INSERT INTO terceros.grupo_info_complementaria (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion) VALUES (18,'Formación académica','Información de la formación académica del aspirante a posgrado','Grupo_18',TRUE, LOCALTIMESTAMP, LOCALTIMESTAMP);")
 	m.SQL("INSERT INTO terceros.info_complementaria (nombre, codigo_abreviacion, activo, grupo_info_complementaria_id, fecha_creacion, fecha_modificacion) VALUES ('AREA_CONOCIMIENTO', 'AREA_CONOCIMIENTO', TRUE, 20, LOCALTIMESTAMP, LOCALTIMESTAMP);")
 	m.SQL("INSERT INTO terceros.info_complementaria (nombre, codigo_abreviacion, activo, grupo_info_complementaria_id, fecha_creacion, fecha_modificacion) VALUES ('FORMACION_ACADEMICA', 'FORMACION_ACADEMICA', TRUE, 20, LOCALTIMESTAMP, LOCALTIMESTAMP);")
 	m.SQL("INSERT INTO terceros.info_complementaria (nombre, codigo_abreviacion, activo, grupo_info_complementaria_id, fecha_creacion, fecha_modificacion) VALUES ('INSTITUCION', 'INSTITUCION', TRUE, 20, LOCALTIMESTAMP, LOCALTIMESTAMP);")
@@ -30,6 +31,8 @@ func (m *InsertTablaInfoComplementaria_20210105_095124) Up() {
 // Reverse the migrations
 func (m *InsertTablaInfoComplementaria_20210105_095124) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
+
 	m.SQL("DELETE FROM terceros.info_complementaria WHERE grupo_info_complementaria_id = 20 ;")
 	m.SQL("DELETE FROM terceros.info_complementaria WHERE nombre = 'NIVEL_FORMACION' and WHERE grupo_info_complementaria_id = 18 ;")
+	m.SQL("DELETE FROM terceros.grupo_info_complementaria WHERE id = 18;")
 }
