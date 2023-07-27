b28d339f8691ec3dbc8aa50603ee6a744f0803c7
lud <ludwinggsanchez@gmail.com>
2023-07-27 03:29:04 -0500

    feat udistrital/arka_cliente#1160: Transacciones identificación
    - Filtro transacción vinculación-identificación
    - Permite filtrar por tipos de vinculación, cargos y dependencias.

--- models/transaccion_identificacion.go
+++ models/transaccion_identificacion.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
 
 type DatosIdentificacionTercero_ struct {
 	Tercero struct {
@@ -16,24 +20,20 @@ type DatosIdentificacionTercero_ struct {
 	}
 }
 
-func GetAllDatosIdentificacionVinculacion(search string, tiposVinculacion string, terceros *[]DatosIdentificacionTercero_) (err error) {
+func GetAllDatosIdentificacionVinculacion(search string, tiposVinculacion, cargos, dependencias string, terceros *[]DatosIdentificacionTercero_) (err error) {
 
 	o := orm.NewOrm()
 
-	search_ := "%" + search + "%"
-	script := scriptVinculaciones(tiposVinculacion)
-	if _, err = o.Raw(script, search_, search_).QueryRows(terceros); err != nil {
-		return err
-	}
-
+	script := scriptVinculaciones(tiposVinculacion, cargos, dependencias)
+	_, err = o.Raw(script, "%"+search+"%").QueryRows(terceros)
 	return
 
 }
 
-func scriptVinculaciones(tipos string) (script string) {
+func scriptVinculaciones(tiposVinculacion, cargos, dependencia string) (script string) {
 
 	script = `
-	WITH con_doc AS (
+	WITH vinculaciones AS (
 		SELECT DISTINCT ON (1)
 			t.id,
 			t.nombre_completo,
@@ -42,73 +42,53 @@ func scriptVinculaciones(tipos string) (script string) {
 			td.codigo_abreviacion,
 			td.id tipo
 		FROM
-			terceros.tercero t,
-			terceros.datos_identificacion di,
-			terceros.tipo_documento td,
 			terceros.vinculacion v,
+			terceros.tercero t
+			INNER JOIN terceros.datos_identificacion di
+				ON di.tercero_id = t.id
+			LEFT JOIN terceros.tipo_documento td
+				ON td.id = di.tipo_documento_id,
 			CONCAT(
 				UPPER(td.codigo_abreviacion), ' ',
 				UPPER(di.numero), ' ',
 				UPPER(t.nombre_completo)
 				) compuesto
 		WHERE
-			v.tercero_principal_id = t.id
-			`
-
-	if len(tipos) > 0 {
-		script += "AND v.tipo_vinculacion_id IN (" + tipos + ")"
-	}
-
-	script += `
-			AND compuesto LIKE UPPER(?)
-			AND di.tercero_id = t.id
-			AND td.id = di.tipo_documento_id
-			AND v.activo = TRUE
-			AND t.activo = TRUE
-			AND di.activo = TRUE
-		ORDER BY 1, 6 DESC
-	), sin_doc AS (
-		SELECT
-			t.id,
-			t.nombre_completo
-		FROM
-			terceros.tercero t,
-			terceros.vinculacion v
-		WHERE
-			UPPER(t.nombre_completo) LIKE UPPER(?)
-			`
-
-	if len(tipos) > 0 {
-		script += "AND v.tipo_vinculacion_id IN (" + tipos + ")"
-	}
-
-	script += `
+				compuesto LIKE UPPER(?)
+			AND t.activo = true
+			AND (di.id is null or di.activo = true)
+			AND (td.id is null or td.codigo_abreviacion != 'CODE')
 			AND v.tercero_principal_id = t.id
-			AND v.activo = TRUE
-			AND t.activo = TRUE
-			AND NOT EXISTS (
-				SELECT 1
-					FROM terceros.datos_identificacion di
-					WHERE di.tercero_id = t.id
-			)
+			COND_TIPO_VINCULACION_ID
+			COND_CARGO_ID
+			COND_DEPENDENCIA_ID
+		ORDER BY 1, 6 DESC
 	)
 
-	SELECT
-		*,
-		NULL AS numero,
-		NULL AS digito_verificacion,
-		NULL AS codigo_abreviacion
-	FROM sin_doc
-	UNION
-	SELECT
-		id,
-		nombre_completo,
-		numero,
-		digito_verificacion,
-		codigo_abreviacion
-	FROM con_doc
-	ORDER BY nombre_completo;
+	SELECT *
+	FROM vinculaciones
+	ORDER BY 2;
 	`
+
+	condVinculacion := ""
+	if len(tiposVinculacion) > 0 {
+		condVinculacion = "AND v.tipo_vinculacion_id IN (" + tiposVinculacion + ")"
+
+	}
+
+	condCargos := ""
+	if len(cargos) > 0 {
+		condCargos = "AND v.cargo_id IN (" + cargos + ")"
+	}
+
+	condDependencia := ""
+	if len(cargos) > 0 {
+		condCargos = "AND v.dependencia_id IN (" + dependencia + ")"
+	}
+
+	script = strings.ReplaceAll(script, "COND_TIPO_VINCULACION_ID", condVinculacion)
+	script = strings.ReplaceAll(script, "COND_CARGO_ID", condCargos)
+	script = strings.ReplaceAll(script, "COND_DEPENDENCIA_ID", condDependencia)
 
 	return
 }
