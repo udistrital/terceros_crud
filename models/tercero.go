6a32914d1cec8f7956636824ffc8ae3ac52b23f7
jupmorenor <jupmorenor@gmail.com>
2026-03-11 16:37:15 -0500

    feat: ajustes implementación de filtro por separado. udistrita./sisifo_documentacion#566

--- models/tercero.go
+++ models/tercero.go
@@ -65,9 +65,14 @@ func GetAllTercero(query map[string]string, fields []string, sortby []string, or
 		k = strings.Replace(k, ".", "__", -1)
 		if strings.Contains(k, "isnull") {
 			qs = qs.Filter(k, (v == "true" || v == "1"))
-		} else if strings.HasSuffix(k, "__in") || strings.HasSuffix(k, "__icontains") {
+		} else if strings.HasSuffix(k, "__in") {
 			arr := strings.Split(v, "|")
 			qs = qs.Filter(k, arr)
+		} else if strings.HasSuffix(k, "__icontains") {
+			words := strings.Split(v, "|")
+			for _, word := range words {
+				qs = qs.Filter(k, word)
+			}
 		} else {
 			qs = qs.Filter(k, v)
 		}
